30e9256e7aef8526dd53d710a9c4fa4460e45068
Chris Kibble <christopher83@gmail.com>
2022-02-23 21:34:30 -0500

    Removed unnecessary verbose output

--- private/Literalize.ps1
+++ private/Literalize.ps1
@@ -12,14 +12,11 @@ Function Literalize {
 
     # Replace all variables with plain text
 
-    Write-Verbose "   Getting all variables from string"
     $VariableList = [Regex]::New("(?msi)\[\[(\w{1,})\]\]").Matches($InputData)
 
     ForEach($V in $VariableList) {        
         $SearchString = $V.Groups[0].Value
         $ReplaceKey = $V.Groups[1].Value
-
-        Write-Verbose "   Completing Find & Replace on '$ReplaceKey'"
 
         $Replacestring = $InputResponse.Where{ $_.Name -eq $ReplaceKey }[0].Val
         
