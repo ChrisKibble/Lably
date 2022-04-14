6c13ecee2e79893871225fd3201936fb49c69b06
Chris Kibble <christopher83@gmail.com>
2022-04-14 07:49:44 -0400

    Changed failure to warning if VM not in Hyper-V

--- public/Remove-LablyVM.ps1
+++ public/Remove-LablyVM.ps1
@@ -37,75 +37,78 @@ Function Remove-LablyVM {
     Try {
         $VM = Get-VM -Id $Asset.VMId -ErrorAction Stop
     } Catch {
-        Throw "Cannot Find VM. $($_.Exception.Message)"
+        Write-Warning "Warning: Cannot Find VM, Skipping. $($_.Exception.Message)"
     }
 
-    If(-Not($Confirm)) {
-        Write-Host "You are about to delete $($VM.Name)"
-        Write-Host "This operation cannot be undone." -ForegroundColor Red
-        Write-Host ""
-        If($(Read-Host "If you're certain you'd like to continue, type DELETE and press enter") -ne "DELETE") {
-            Write-Host "Code 'DELETE' was not entered. Aborting." -ForegroundColor Yellow
-            Return
+    If($VM) {
+        
+        If(-Not($Confirm)) {
+            Write-Host "You are about to delete $($VM.Name)"
+            Write-Host "This operation cannot be undone." -ForegroundColor Red
+            Write-Host ""
+            If($(Read-Host "If you're certain you'd like to continue, type DELETE and press enter") -ne "DELETE") {
+                Write-Host "Code 'DELETE' was not entered. Aborting." -ForegroundColor Yellow
+                Return
+            }
         }
-    }
 
-    Write-Host "Stopping VM"
-    Try {
-        $VM | Stop-VM -Force -TurnOff -ErrorAction Stop -WarningAction SilentlyContinue
-    } Catch {
-        Throw "Could not stop VM. $($_.Exception.Message)"
-    }
+        Write-Host "Stopping VM"
+        Try {
+            $VM | Stop-VM -Force -TurnOff -ErrorAction Stop -WarningAction SilentlyContinue
+        } Catch {
+            Throw "Could not stop VM. $($_.Exception.Message)"
+        }
 
-    Write-Verbose "State of $($VM.Name) is $($VM.State). Status is $($VM.Status)."
+        Write-Verbose "State of $($VM.Name) is $($VM.State). Status is $($VM.Status)."
 
-    While($VM.State -ne [Microsoft.HyperV.PowerShell.VMState]::Off -and $VM.Status -ne "Operating Normally") {
-        Write-Verbose "Waiting for VM to Stop (State=$($VM.State) Status=$($VM.Status))..."
-        Start-Sleep -Seconds 1
-    }
+        While($VM.State -ne [Microsoft.HyperV.PowerShell.VMState]::Off -and $VM.Status -ne "Operating Normally") {
+            Write-Verbose "Waiting for VM to Stop (State=$($VM.State) Status=$($VM.Status))..."
+            Start-Sleep -Seconds 1
+        }
 
-    $ActivityStart = Get-Date
-    $RunTime = New-TimeSpan -Start $ActivityStart -End (Get-Date)
-    While(@($VM | Get-VMHardDiskDrive | Select-Object -ExpandProperty Path) -like "*.avhdx" -or $RunTime.TotalMinutes -ge 1) {
-        Write-Verbose "Waiting for Disk Merging Activities to Complete ..."
-        Start-Sleep -Seconds 1
+        $ActivityStart = Get-Date
         $RunTime = New-TimeSpan -Start $ActivityStart -End (Get-Date)
+        While(@($VM | Get-VMHardDiskDrive | Select-Object -ExpandProperty Path) -like "*.avhdx" -or $RunTime.TotalMinutes -ge 1) {
+            Write-Verbose "Waiting for Disk Merging Activities to Complete ..."
+            Start-Sleep -Seconds 1
+            $RunTime = New-TimeSpan -Start $ActivityStart -End (Get-Date)
+        }
+
+        $VHDPaths = @()
+
+        $VM | Get-VMHardDiskDrive | ForEach-Object { 
+            Write-Host "Deleting $($_.Path) ..."
+        
+            $VHDPaths += Split-Path $_.Path
+
+            $AttemptNumber = 0
+            $MaxAttempts = 5
+
+            Do {
+                Try {
+                    $FileDeleteSuccess = $False
+                    $AttemptNumber++
+                    Remove-Item $($_.Path) -Force -ErrorAction Stop
+                    $FileDeleteSuccess = $True
+                } Catch {
+                    # In edge cases, VHDx is still merging even though status doesn't seem to show that's the case. The file
+                    # appears to be held in use for an extra second or so.
+                    Write-Warning "Could not delete $($_.Path) (Attempt $AttemptNumber of $MaxAttempts)."
+                    If($Attempt -eq $MaxAttempt) { Write-Warning $($_.Exception.Message) }
+                    Start-Sleep -Seconds 5
+                }    
+            } Until ($FileDeleteSuccess -or $AttemptNumber -eq $MaxAttempts)
+        }
+
+        Write-Host "Removing VM $($VM.Name)"
+
+        Try {
+            $VM | Remove-VM -Force
+        } Catch {
+            Write-Warning "Could not delete $($VM.Name). $($_.Exception.Message)"            
+        }      
     }
-
-    $VHDPaths = @()
-
-    $VM | Get-VMHardDiskDrive | ForEach-Object { 
-        Write-Host "Deleting $($_.Path) ..."
     
-        $VHDPaths += Split-Path $_.Path
-
-        $AttemptNumber = 0
-        $MaxAttempts = 5
-
-        Do {
-            Try {
-                $FileDeleteSuccess = $False
-                $AttemptNumber++
-                Remove-Item $($_.Path) -Force -ErrorAction Stop
-                $FileDeleteSuccess = $True
-            } Catch {
-                # In edge cases, VHDx is still merging even though status doesn't seem to show that's the case. The file
-                # appears to be held in use for an extra second or so.
-                Write-Warning "Could not delete $($_.Path) (Attempt $AttemptNumber of $MaxAttempts)."
-                If($Attempt -eq $MaxAttempt) { Write-Warning $($_.Exception.Message) }
-                Start-Sleep -Seconds 5
-            }    
-        } Until ($FileDeleteSuccess -or $AttemptNumber -eq $MaxAttempts)
-    }
-
-    Write-Host "Removing VM $($VM.Name)"
-
-    Try {
-        $VM | Remove-VM -Force
-    } Catch {
-        Write-Warning "Could not delete $($VM.Name). $($_.Exception.Message)"            
-    }      
-
     ForEach($VHDPath in $VHDPaths) {
         Write-Verbose "Clearing $VHDPath (If Empty)"
         If(-Not (Get-ChildItem $VHDPath | Select-Object -First 1)) { Remove-Item $VHDPath -ErrorAction SilentlyContinue }
