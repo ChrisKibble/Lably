acb2646546ebacbea88242865682281edb56f032
Chris Kibble <christopher83@gmail.com>
2022-04-27 07:18:48 -0400

    Added warning and sample ISO path.

--- pester/Invoke-Tests.Sample.ps1
+++ pester/Invoke-Tests.Sample.ps1
@@ -1,5 +1,8 @@
 # This needs to point to a valid ISO for Testing Lably
-$env:LablyISO = ''
+$env:LablyISO = 'C:\ISOs\en_windows_server_version_20h2_updated_jun_2021_x64_dvd_8ca193c2.iso'
 
-# Run Pester
+Write-Host "Warning: If a test fails, it may leave behind Lably data, VM Switches, Network IPs, etc. that need to be manually cleaned up." -ForegroundColor Red
+
+Start-Sleep -Seconds 5
+
 Invoke-Pester -Path $PSScriptRoot
