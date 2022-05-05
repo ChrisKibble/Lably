838630b7f3023b148d01202c12d71545cd63fe85
Chris Kibble <christopher83@gmail.com>
2022-05-05 18:24:35 -0500

    Created/Updated Credits

--- module/Lably/public/New-LablyBaseVHD.ps1
+++ module/Lably/public/New-LablyBaseVHD.ps1
@@ -64,8 +64,6 @@ Function New-LablyBaseVHD {
 
     ValidateModuleRun -RequiresAdministrator
 
-    # Credit: Help with this function from: https://github.com/greyhamwoohoo/new-vm-from-iso
-
     $gptTypeEFI = "{c12a7328-f81f-11d2-ba4b-00a0c93ec93b}"
     $gptTypeMSR = "{e3c9e316-0b5c-4db8-817d-f92df00215ae}"
 
